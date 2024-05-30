f517d47406684d7bd5019f661818294cfc4feb62
Katia Cherdoneva <32202527+katiacherdoneva@users.noreply.github.com>
2024-05-30 13:43:29 +0300

    Refactoring the web api.

--- Web.Api/Models/PairEmployeesModel.cs
+++ Web.Api/Models/PairEmployeesModel.cs
@@ -3,8 +3,11 @@
     public class PairEmployeesModel
     {
         public int EmpId1 { get; set; }
+
         public int EmpId2 { get; set; }
+
         public int ProjectId { get; set; }
+
         public long DaysWorked { get; set; }
     }
 }
--- Web.Api/Utils/CalculateDatetime.cs
+++ Web.Api/Utils/CalculateDatetime.cs
@@ -2,13 +2,8 @@
 {
     public static class CalculateDatetime
     {
-        public static int Overlap(DateTime e1d1, DateTime e1d2, DateTime e2d1, DateTime e2d2)
+        public static int Overlap(DateTime startDate1, DateTime endDate1, DateTime startDate2, DateTime endDate2)
         {
-            DateTime startDate1 = e1d1;
-            DateTime endDate1 = e1d2 == null ? DateTime.Now : e1d2;
-            DateTime startDate2 = e2d1;
-            DateTime endDate2 = e2d2 == null ? DateTime.Now : e2d2;
-
             DateTime start = startDate1 < startDate2 ? startDate2 : startDate1;
             DateTime end = endDate1 < endDate2 ? endDate1 : endDate2;
 
